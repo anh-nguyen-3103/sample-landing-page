b326f8d2cc00423074b9b8ad443afabeaee64829
anhnguyen <ngocanh310301@gmail.com>
2025-04-17 10:25:58 +0700

    feat(code): init db jobs

--- src/app/(frontend)/careers/page.tsx
+++ src/app/(frontend)/careers/page.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
+import configPromise from '@payload-config'
+import { getPayload } from 'payload'
 
 export default async function Page() {
-  return <div className="w-full h-[100vh] container mx-auto" />
+  const payload = await getPayload({ config: configPromise })
+  const { docs } = await payload.find({ collection: 'jobs' })
+
+  console.log(docs)
+
+  return <div className="w-full h-[100vh] container mx-auto"></div>
 }
